5d88cd4b9b36f0c001e70c619862f08f61cfbe4b
Gabriela-tech54 <gabrielaborgesdesousa714@gmail.com>
2025-10-01 15:23:46 -0300

    transferindo as telas para o novo repositorio

---
+++ PHP/teste.php
@@ -0,0 +1,40 @@
+<?php
+
+// este é um comentário
+# este também é um cometário
+/*este é um comentário 
+de varias 
+linhas
+*/
+
+/* O QUE É UMA VÁRIAVEL?
+É um espaço que criamos para armazenar dados, que pode
+ser um texto, um numero, um arquivo, uma data, um 
+conjunto de dados, ect.
+*/
+$nome = "gabriela"; // guarda strings/textos
+$idade = 16; // guardar numeros int / inteiros
+$altura = 1.63; // guardar valores quebrados (DOUBLE)
+$fim_de_semana_chegou = true;
+//valores booleanos (true,false)
+
+
+/*O que é uma condicional?
+é quando para uma ação ser executada um critério deve
+ser atendido:
+exemplo. Se não chover eu irei ao parque cimba, senão
+eu irei ao mobcine.
+--> hoje choveu! então eu irei ao mobicine
+--> hoje não choveu! então eu irei ao parque cimba
+*/ 
+$idadePessoa = 18;
+
+// se pessoa for maior ou igual a 18 anos
+if($idadePessoa>=18){
+    //então é maior de idade
+}else{
+    // não é maior de idade
+}
+
+// fechando o php
+?> 
